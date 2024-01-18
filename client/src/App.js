3493daad3c0c6622bd9cd8108f2e2e0758b3560a
Anthony Wang <anthonywang64@gmail.com>
2024-01-18 15:05:15 -0500

    added react router functionality and routes to page paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Overview from './pages/Overview';
 
 function App() {
   return (
-    <div className="App">
-      hello there
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/overview" element={<Overview />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Overview.jsx
@@ -0,0 +1,7 @@
+export default function Overview() {
+    return (
+        <div>
+            <h1>Overview</h1>
+        </div>
+    );
+}
