a52c79d028df9625e03c8b0bc4d590ab51725e25
Anthony Wang <anthonywang64@gmail.com>
2024-01-18 15:21:05 -0500

    downloaded react mui and react-toastify; created a simple sign-up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import { TextField } from '@mui/material/';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function SignUp() {
+    // Define user state and isLoading state using useState hook
+    const [user, setUser] = useState({ email: '', password: '', error: null });
+
+    // Determine if the form is valid by checking if email and password are empty
+    const isValid = user.email === '' || user.password === '';
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    // Handle change event on input fields
+    const handleChange = e => {
+        // Set user state with updated value
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    // Handle submit event on form
+    const handleSubmit = async e => {
+
+    }
+
+
+
     return (
-        <div>
-            <h1>Sign Up</h1>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: -20,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: '#64dd17' }}>
+
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        value={user.email}
+                        autoFocus
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        margin="normal"
+                        placeholder=''
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        onChange={handleChange}
+                    />
+                    <Button
+                        type="submit"
+                        onClick={handleSubmit}
+                        disabled={isLoading || isValid}
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/sign-up" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+            {/*TOASTER CONTAINER SO TOASTS CAN DISPLAY */}
+            <ToastContainer
+                position="top-right"
+                autoClose={2500}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     );
 }
