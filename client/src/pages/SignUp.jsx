00ad5c26b45629a6895af1d236548060e71ce6ca
Anthony Wang <anthonywang64@gmail.com>
2024-01-18 15:27:36 -0500

    created simple sign-in page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,39 +2,34 @@ import React, { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import { TextField } from '@mui/material/';
+import { TextField } from '@mui/material';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 export default function SignUp() {
-    // Define user state and isLoading state using useState hook
-    const [user, setUser] = useState({ email: '', password: '', error: null });
+    const initialUser = { id: null, firstName: '', lastName: '', email: '', password: '', error: null }
 
-    // Determine if the form is valid by checking if email and password are empty
-    const isValid = user.email === '' || user.password === '';
-
+    const [user, setUser] = useState(initialUser);
     const [isLoading, setIsLoading] = useState(false);
 
     const navigate = useNavigate();
 
-    // Handle change event on input fields
+    // Handle change in form fields
     const handleChange = e => {
-        // Set user state with updated value
-        setUser({ ...user, [e.target.name]: e.target.value })
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value })
     }
 
     // Handle submit event on form
-    const handleSubmit = async e => {
+    const handleSubmit = async (event) => {
 
     }
 
-
+    const isValid = user.firstName === '' || user.lastName === '' || user.email === '' || user.password === '';
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
@@ -42,76 +37,85 @@ export default function SignUp() {
                 <CssBaseline />
                 <Box
                     sx={{
-                        marginTop: -20,
+                        marginTop: -15,
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
                     }}
                 >
                     <Avatar sx={{ m: 1, bgcolor: '#64dd17' }}>
-
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                        Sign in
+                        Sign up
                     </Typography>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        value={user.email}
-                        autoFocus
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        margin="normal"
-                        placeholder=''
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        onChange={handleChange}
-                    />
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                margin="normal"
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                value={user.firstName}
+                                autoFocus
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="lastName"
+                                value={user.lastName}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                value={user.email}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                    </Grid>
                     <Button
                         type="submit"
-                        onClick={handleSubmit}
-                        disabled={isLoading || isValid}
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2 }}
+                        disabled={isLoading || isValid}
+                        onClick={handleSubmit}
                     >
-                        Sign In
+                        Sign Up
                     </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
-                            </Link>
-                        </Grid>
+                    <Grid container justifyContent="flex-end">
                         <Grid item>
-                            <Link href="/sign-up" variant="body2">
-                                {"Don't have an account? Sign Up"}
+                            <Link href="/sign-in" variant="body2">
+                                Already have an account? Sign in
                             </Link>
                         </Grid>
                     </Grid>
                 </Box>
             </Container>
-            {/*TOASTER CONTAINER SO TOASTS CAN DISPLAY */}
-            <ToastContainer
-                position="top-right"
-                autoClose={2500}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
         </div>
     );
 }
