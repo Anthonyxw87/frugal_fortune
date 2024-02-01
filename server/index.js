b14cc28cd8201d8236229d3e2fc7ddeee7818c5f
Anthony Wang <anthonywang64@gmail.com>
2024-02-01 16:44:41 -0500

    fixed dockerfile for server so it works now

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
+    res.send('Hello World');
 });
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
